5a2581754f2bff92d4fd673441673d8ef910527c
sabella <70499164+sabellapr@users.noreply.github.com>
2020-12-20 22:57:18 +0700

    view database
    view fix

--- murid.blade.php
+++ murid.blade.php
@@ -2,36 +2,35 @@
 @section('title', 'View Data')
    
 @section('content')
-<main role="main">
-<div class="jumbotron text-white rounded bg-dark">
-<div class="container">
+<section class="jumbotron text-white rounded bg-dark">
+    <div class="container">
     <div class="col-sm-12">
         <h1 class="display-6 mt-5 font-italic text-center">View Data</h1>
     </div>
 </div>
-</div>
-<div class="row">
-<div class="container">
-    <div class="col-sm-12">
+</section>
+<main role="main" class="container">
+    <div class="row">
+      <div class="col-md-12 blog-main">
 <table class="table table table-bordered">
     <thead>
       <tr>
         <th scope="col">No</th>
-        <th scope="col">nis</th>
-        <th scope="col">nama</th>
-        <th scope="col">jurusan</th>
+        <th scope="col">Nis</th>
+        <th scope="col">Nama</th>
+        <th scope="col">Jurusan</th>
       </tr>  
     </thead>
     <tbody>
         @foreach ($dataa as $item)
       <tr>
-        <th scope="row">1</th>
+        <th scope="row">{{ $loop->iteration }}</th>
         <td>{{ $item->nis }} </td>
         <td>{{ $item->nama }} </td>
         <td>{{ $item->jurusan }}</td>
       </tr>
       <tr>
-        <th scope="row">2</th>
+        <th scope="row">{{ $loop->iteration }}</th>
         <td>{{ $item->nis }} </td>
         <td>{{ $item->nama }} </td>
         <td>{{ $item->jurusan }} </td>
